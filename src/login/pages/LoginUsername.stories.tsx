15dd94d7f9cc71d58d7e6f08fb264e521502e84d
Nathanaël M. <Nathanael-Mtd@users.noreply.github.com>
2025-02-26 19:21:41 +0100

    feat: Add LoginUsername theme compatibility (#42)
    * feat: Add LoginUsername theme compatibility
    
    * fix: invalid headerNode
    
    * chore: minor cleanups
    
    ---------
    
    Co-authored-by: paulwer <paul@wer-ner.de>

--- src/login/pages/LoginUsername.stories.tsx
+++ src/login/pages/LoginUsername.stories.tsx
@@ -28,3 +28,249 @@ export const WithEmailAsUsername: Story = {
         />
     )
 };
+
+export const WithoutRegistration: Story = {
+    render: () => (
+        <KcPageStory
+            kcContext={{
+                realm: { registrationAllowed: false }
+            }}
+        />
+    )
+};
+
+export const WithoutRememberMe: Story = {
+    render: () => (
+        <KcPageStory
+            kcContext={{
+                realm: { rememberMe: false }
+            }}
+        />
+    )
+};
+
+export const WithSocialProviders: Story = {
+    render: () => (
+        <KcPageStory
+            kcContext={{
+                social: {
+                    displayInfo: true,
+                    providers: [
+                        {
+                            loginUrl: "google",
+                            alias: "google",
+                            providerId: "google",
+                            displayName: "Google",
+                            iconClasses: "fa fa-google"
+                        },
+                        {
+                            loginUrl: "microsoft",
+                            alias: "microsoft",
+                            providerId: "microsoft",
+                            displayName: "Microsoft",
+                            iconClasses: "fa fa-windows"
+                        },
+                        {
+                            loginUrl: "facebook",
+                            alias: "facebook",
+                            providerId: "facebook",
+                            displayName: "Facebook",
+                            iconClasses: "fa fa-facebook"
+                        },
+                        {
+                            loginUrl: "instagram",
+                            alias: "instagram",
+                            providerId: "instagram",
+                            displayName: "Instagram",
+                            iconClasses: "fa fa-instagram"
+                        },
+                        {
+                            loginUrl: "twitter",
+                            alias: "twitter",
+                            providerId: "twitter",
+                            displayName: "Twitter",
+                            iconClasses: "fa fa-twitter"
+                        },
+                        {
+                            loginUrl: "linkedin",
+                            alias: "linkedin",
+                            providerId: "linkedin",
+                            displayName: "LinkedIn",
+                            iconClasses: "fa fa-linkedin"
+                        },
+                        {
+                            loginUrl: "stackoverflow",
+                            alias: "stackoverflow",
+                            providerId: "stackoverflow",
+                            displayName: "Stackoverflow",
+                            iconClasses: "fa fa-stack-overflow"
+                        },
+                        {
+                            loginUrl: "github",
+                            alias: "github",
+                            providerId: "github",
+                            displayName: "Github",
+                            iconClasses: "fa fa-github"
+                        },
+                        {
+                            loginUrl: "gitlab",
+                            alias: "gitlab",
+                            providerId: "gitlab",
+                            displayName: "Gitlab",
+                            iconClasses: "fa fa-gitlab"
+                        },
+                        {
+                            loginUrl: "bitbucket",
+                            alias: "bitbucket",
+                            providerId: "bitbucket",
+                            displayName: "Bitbucket",
+                            iconClasses: "fa fa-bitbucket"
+                        },
+                        {
+                            loginUrl: "paypal",
+                            alias: "paypal",
+                            providerId: "paypal",
+                            displayName: "PayPal",
+                            iconClasses: "fa fa-paypal"
+                        },
+                        {
+                            loginUrl: "openshift",
+                            alias: "openshift",
+                            providerId: "openshift",
+                            displayName: "OpenShift",
+                            iconClasses: "fa fa-cloud"
+                        }
+                    ]
+                }
+            }}
+        />
+    )
+};
+
+export const WithOneSocialProvider: Story = {
+    render: args => (
+        <KcPageStory
+            {...args}
+            kcContext={{
+                social: {
+                    displayInfo: true,
+                    providers: [
+                        {
+                            loginUrl: "google",
+                            alias: "google",
+                            providerId: "google",
+                            displayName: "Google",
+                            iconClasses: "fa fa-google"
+                        }
+                    ]
+                }
+            }}
+        />
+    )
+};
+
+export const WithTwoSocialProviders: Story = {
+    render: args => (
+        <KcPageStory
+            {...args}
+            kcContext={{
+                social: {
+                    displayInfo: true,
+                    providers: [
+                        {
+                            loginUrl: "google",
+                            alias: "google",
+                            providerId: "google",
+                            displayName: "Google",
+                            iconClasses: "fa fa-google"
+                        },
+                        {
+                            loginUrl: "microsoft",
+                            alias: "microsoft",
+                            providerId: "microsoft",
+                            displayName: "Microsoft",
+                            iconClasses: "fa fa-windows"
+                        }
+                    ]
+                }
+            }}
+        />
+    )
+};
+export const WithNoSocialProviders: Story = {
+    render: args => (
+        <KcPageStory
+            {...args}
+            kcContext={{
+                social: {
+                    displayInfo: true,
+                    providers: []
+                }
+            }}
+        />
+    )
+};
+export const WithMoreThanTwoSocialProviders: Story = {
+    render: args => (
+        <KcPageStory
+            {...args}
+            kcContext={{
+                social: {
+                    displayInfo: true,
+                    providers: [
+                        {
+                            loginUrl: "google",
+                            alias: "google",
+                            providerId: "google",
+                            displayName: "Google",
+                            iconClasses: "fa fa-google"
+                        },
+                        {
+                            loginUrl: "microsoft",
+                            alias: "microsoft",
+                            providerId: "microsoft",
+                            displayName: "Microsoft",
+                            iconClasses: "fa fa-windows"
+                        },
+                        {
+                            loginUrl: "facebook",
+                            alias: "facebook",
+                            providerId: "facebook",
+                            displayName: "Facebook",
+                            iconClasses: "fa fa-facebook"
+                        },
+                        {
+                            loginUrl: "twitter",
+                            alias: "twitter",
+                            providerId: "twitter",
+                            displayName: "Twitter",
+                            iconClasses: "fa fa-twitter"
+                        }
+                    ]
+                }
+            }}
+        />
+    )
+};
+export const WithSocialProvidersAndWithoutRememberMe: Story = {
+    render: args => (
+        <KcPageStory
+            {...args}
+            kcContext={{
+                social: {
+                    displayInfo: true,
+                    providers: [
+                        {
+                            loginUrl: "google",
+                            alias: "google",
+                            providerId: "google",
+                            displayName: "Google",
+                            iconClasses: "fa fa-google"
+                        }
+                    ]
+                },
+                realm: { rememberMe: false }
+            }}
+        />
+    )
+};
--- src/login/pages/LoginUsername.tsx
+++ src/login/pages/LoginUsername.tsx
@@ -1,9 +1,11 @@
 import { useState } from "react";
+import { kcSanitize } from "keycloakify/lib/kcSanitize";
 import { clsx } from "keycloakify/tools/clsx";
 import { getKcClsx } from "keycloakify/login/lib/kcClsx";
 import type { PageProps } from "keycloakify/login/pages/PageProps";
 import type { KcContext } from "../KcContext";
 import type { I18n } from "../i18n";
+import useProviderLogos from "../useProviderLogos";
 
 export default function LoginUsername(props: PageProps<Extract<KcContext, { pageId: "login-username.ftl" }>, I18n>) {
     const { kcContext, i18n, doUseDefaultCss, Template, classes } = props;
@@ -19,6 +21,8 @@ export default function LoginUsername(props: PageProps<Extract<KcContext, { page
 
     const [isLoginButtonDisabled, setIsLoginButtonDisabled] = useState(false);
 
+    const providerLogos = useProviderLogos();
+
     return (
         <Template
             kcContext={kcContext}
@@ -26,44 +30,60 @@ export default function LoginUsername(props: PageProps<Extract<KcContext, { page
             doUseDefaultCss={doUseDefaultCss}
             classes={classes}
             displayMessage={!messagesPerField.existsError("username")}
+            headerNode={msg("doLogIn")}
             displayInfo={realm.password && realm.registrationAllowed && !registrationDisabled}
             infoNode={
-                <div id="kc-registration" className={"text-center"}>
-                    <span>
-                        {msg("noAccount")}{" "}
-                        <a
-                            tabIndex={6}
-                            href={url.registrationUrl}
-                            className={"text-primary-600 hover:text-primary-500 inline-flex no-underline hover:no-underline"}
-                        >
-                            {msg("doRegister")}
-                        </a>
-                    </span>
+                <div id="kc-registration-container" className={"space-y-4"}>
+                    <div id="kc-registration" className={"text-center"}>
+                        <span>
+                            {msg("noAccount")}{" "}
+                            <a
+                                tabIndex={8}
+                                href={url.registrationUrl}
+                                className={"text-primary-600 hover:text-primary-500 inline-flex no-underline hover:no-underline"}
+                            >
+                                {msg("doRegister")}
+                            </a>
+                        </span>
+                    </div>
                 </div>
             }
-            headerNode={msg("doLogIn")}
             socialProvidersNode={
                 <>
                     {realm.password && social?.providers !== undefined && social.providers.length !== 0 && (
                         <div id="kc-social-providers" className={kcClsx("kcFormSocialAccountSectionClass")}>
                             <hr />
-                            <h2>{msg("identity-provider-login-label")}</h2>
-                            <ul className={kcClsx("kcFormSocialAccountListClass", social.providers.length > 3 && "kcFormSocialAccountListGridClass")}>
+                            <h2 className={"pt-4 separate text-secondary-600 text-sm"}>{msg("identity-provider-login-label")}</h2>
+                            <ul
+                                className={clsx(
+                                    kcClsx("kcFormSocialAccountListClass", social.providers.length > 3 && "kcFormSocialAccountListGridClass"),
+                                    "gap-4 grid grid-cols-3 pt-4"
+                                )}
+                            >
                                 {social.providers.map((...[p, , providers]) => (
                                     <li key={p.alias}>
                                         <a
                                             id={`social-${p.alias}`}
-                                            className={kcClsx(
-                                                "kcFormSocialAccountListButtonClass",
-                                                providers.length > 3 && "kcFormSocialAccountGridItem"
+                                            className={clsx(
+                                                kcClsx("kcFormSocialAccountListButtonClass", providers.length > 3 && "kcFormSocialAccountGridItem"),
+                                                `border border-secondary-200 flex justify-center py-2 rounded-lg hover:border-opacity-30 hover:bg-provider-${p.alias}/10`
                                             )}
                                             type="button"
                                             href={p.loginUrl}
                                         >
-                                            {p.iconClasses && <i className={clsx(kcClsx("kcCommonLogoIdP"), p.iconClasses)} aria-hidden="true"></i>}
-                                            <span className={clsx(kcClsx("kcFormSocialAccountNameClass"), p.iconClasses && "kc-social-icon-text")}>
-                                                {p.displayName}
-                                            </span>
+                                            <div className={"h-6 w-6"}>
+                                                {providerLogos[p.alias] ? (
+                                                    <img src={providerLogos[p.alias]} alt={`${p.displayName} logo`} className={"h-full w-auto"} />
+                                                ) : (
+                                                    // Fallback to the original iconClasses if the logo is not defined
+                                                    p.iconClasses && (
+                                                        <i
+                                                            className={clsx(kcClsx("kcCommonLogoIdP"), p.iconClasses, `text-provider-${p.alias}`)}
+                                                            aria-hidden="true"
+                                                        ></i>
+                                                    )
+                                                )}
+                                            </div>
                                         </a>
                                     </li>
                                 ))}
@@ -74,7 +94,7 @@ export default function LoginUsername(props: PageProps<Extract<KcContext, { page
             }
         >
             <div id="kc-form">
-                <div id="kc-form-wrapper">
+                <div id="kc-form-wrapper" className={"space-y-4"}>
                     {realm.password && (
                         <form
                             id="kc-form-login"
@@ -84,6 +104,7 @@ export default function LoginUsername(props: PageProps<Extract<KcContext, { page
                             }}
                             action={url.loginAction}
                             method="post"
+                            className={"m-0 space-y-4"}
                         >
                             {!usernameHidden && (
                                 <div className={kcClsx("kcFormGroupClass")}>
@@ -91,16 +112,16 @@ export default function LoginUsername(props: PageProps<Extract<KcContext, { page
                                         {!realm.loginWithEmailAllowed
                                             ? msg("username")
                                             : !realm.registrationEmailAsUsername
-                                              ? msg("usernameOrEmail")
-                                              : msg("email")}
+                                                ? msg("usernameOrEmail")
+                                                : msg("email")}
                                     </label>
                                     <input
                                         placeholder={
                                             !realm.loginWithEmailAllowed
                                                 ? msgStr("username")
                                                 : !realm.registrationEmailAsUsername
-                                                  ? msgStr("usernameOrEmail")
-                                                  : msgStr("email")
+                                                    ? msgStr("usernameOrEmail")
+                                                    : msgStr("email")
                                         }
                                         tabIndex={2}
                                         id="username"
@@ -112,13 +133,18 @@ export default function LoginUsername(props: PageProps<Extract<KcContext, { page
                                         defaultValue={login.username ?? ""}
                                         type="text"
                                         autoFocus
-                                        autoComplete="off"
+                                        autoComplete="username"
                                         aria-invalid={messagesPerField.existsError("username")}
                                     />
                                     {messagesPerField.existsError("username") && (
-                                        <span id="input-error" className={kcClsx("kcInputErrorMessageClass")} aria-live="polite">
-                                            {messagesPerField.getFirstError("username")}
-                                        </span>
+                                        <span
+                                            id="input-error"
+                                            className={kcClsx("kcInputErrorMessageClass")}
+                                            aria-live="polite"
+                                            dangerouslySetInnerHTML={{
+                                                __html: kcSanitize(messagesPerField.getFirstError("username"))
+                                            }}
+                                        />
                                     )}
                                 </div>
                             )}
@@ -129,10 +155,11 @@ export default function LoginUsername(props: PageProps<Extract<KcContext, { page
                                         <div className="checkbox">
                                             <label>
                                                 <input
-                                                    tabIndex={3}
+                                                    tabIndex={5}
                                                     id="rememberMe"
                                                     name="rememberMe"
                                                     type="checkbox"
+                                                    className={"accent-primary-600"}
                                                     defaultChecked={!!login.rememberMe}
                                                 />{" "}
                                                 {msg("rememberMe")}
